250efc0a39d088966850294156dab2be106b1620
jylim <jylim@crscube.co.kr>
2024-02-27 08:23:30 +0900

    first commit

---
+++ src/main/kotlin/com/agent/agentemployment/AgentEmploymentApplication.kt
@@ -0,0 +1,11 @@
+package com.agent.agentemployment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgentEmploymentApplication
+
+fun main(args: Array<String>) {
+    runApplication<AgentEmploymentApplication>(*args)
+}
---
+++ src/test/kotlin/com/agent/agentemployment/AgentEmploymentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.agent.agentemployment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgentEmploymentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
