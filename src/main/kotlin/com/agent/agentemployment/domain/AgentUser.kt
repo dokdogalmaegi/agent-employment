cbcbc8485b7e15df24142388b07d5a015bb5035b
dokdogalmaegi <clik202012@gmail.com>
2024-02-29 20:01:18 +0900

    Add AgentUser

---
+++ src/main/kotlin/com/agent/agentemployment/domain/AgentUser.kt
@@ -0,0 +1,33 @@
+package com.agent.agentemployment.domain
+
+import com.agent.agentemployment.domain.enums.UserRole
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+
+class AgentUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    val id: Long = 0L
+
+    var name: String = ""
+
+    @Column(unique = true)
+    var username: String = ""
+
+    var password: String = ""
+
+    var isAdmin: Boolean = false
+
+    @delegate:Transient
+    val roles: Set<UserRole> by lazy {
+        buildSet {
+            add(UserRole.USER)
+            if (isAdmin) add(UserRole.ADMIN)
+        }
+    }
+}
