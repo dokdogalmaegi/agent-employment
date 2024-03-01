c379f1e897de654c548ac2fc361e305a5a380810
dokdogalmaegi <clik202012@gmail.com>
2024-03-01 11:42:18 +0900

    Add AgentPrincipalTest

--- src/main/kotlin/com/agent/agentemployment/domain/AgentUser.kt
+++ src/main/kotlin/com/agent/agentemployment/domain/AgentUser.kt
@@ -6,9 +6,12 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import lombok.AllArgsConstructor
+import lombok.RequiredArgsConstructor
 
 @Entity
-
+@AllArgsConstructor
+@RequiredArgsConstructor
 class AgentUser {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
---
+++ src/test/kotlin/com/agent/agentemployment/security/session/AgentPrincipalTest.kt
@@ -0,0 +1,39 @@
+package com.agent.agentemployment.security.session
+
+import com.agent.agentemployment.domain.AgentUser
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class AgentPrincipalTest {
+
+    @Test
+    fun getAuthorities() {
+    }
+
+    @Test
+    fun getPassword() {
+    }
+
+    @Test
+    fun getUsername() {
+    }
+
+    @Test
+    fun isAccountNonExpired() {
+    }
+
+    @Test
+    fun isAccountNonLocked() {
+    }
+
+    @Test
+    fun isCredentialsNonExpired() {
+    }
+
+    @Test
+    fun isEnabled() {
+        Assertions.assertThat(true).isTrue()
+    }
+}
