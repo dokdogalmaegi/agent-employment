9c32e8e46ca8ac2dc8fe4bb726279922b0ed6edf
dokdogalmaegi <clik202012@gmail.com>
2024-03-02 19:48:13 +0900

    format

--- src/main/kotlin/com/agent/agentemployment/domain/service/impl/UserServiceImpl.kt
+++ src/main/kotlin/com/agent/agentemployment/domain/service/impl/UserServiceImpl.kt
@@ -1,11 +1,14 @@
 package com.agent.agentemployment.domain.service.impl
 
+import com.agent.agentemployment.domain.persistence.AgentUserRepository
 import com.agent.agentemployment.domain.service.UserService
 import com.agent.agentemployment.dto.UserSignUpDTO
 import org.springframework.stereotype.Service
 
 @Service
-class UserServiceImpl: UserService {
+class UserServiceImpl(
+    private val userRepository: AgentUserRepository
+) : UserService {
     override fun createUser(userSignUpDTO: UserSignUpDTO) {
         TODO("Not yet implemented")
     }
--- src/main/kotlin/com/agent/agentemployment/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/agent/agentemployment/security/WebSecurityConfig.kt
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
